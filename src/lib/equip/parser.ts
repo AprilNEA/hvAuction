d1fa36851bc47b42f2db000db9f7c56930a18148
SukkaW <isukkaw@gmail.com>
2021-04-17 08:48:28 +0800

    Fix: bbcode for Slaughter Rapier

--- src/lib/equip/parser.ts
+++ src/lib/equip/parser.ts
@@ -365,7 +365,7 @@ function generateBBCode(equipInfo: ReturnType<typeof parseEquipmentFromDocument>
   const suffix: BBCodeSnippetType = { code: equipInfo?.suffix };
 
   if (equipInfo?.category === '1H') {
-    if (equipInfo?.name.includes('Rapier') && equipInfo.suffix === 'suffix') {
+    if (equipInfo?.name.includes('Rapier') && equipInfo.suffix === 'Slaughter') {
       if (equipInfo.prefix === 'Ethereal' || equipInfo.prefix === 'Hallowed' || equipInfo.prefix === 'Demonic') {
         prefix.color = color1;
         type.bold = true;
@@ -486,7 +486,9 @@ function generateBBCode(equipInfo: ReturnType<typeof parseEquipmentFromDocument>
         i.code = `[b][color=${i.color}]${i.code}[/color][/b]`;
       }
     } else if (i.bold) {
-      i.code = `[b]${i.code}[/b]`;
+      if (i.code !== '') {
+        i.code = `[b]${i.code}[/b]`;
+      }
     }
     if (i.the) i.code = `the ${i.code}`;
   });
